dede32fea19449e45b06dfd09696fb6f1604a68c
YUKIKO <rashifamido@gmail.com>
2014-10-01 22:23:23 +0800

    10/1 bootstrap適用

--- php/message/index.php
+++ php/message/index.php
@@ -22,9 +22,9 @@
         function Check($name, $contents, &$errors, $error_name, $count){
             $rec = null;
             if(!isset($contents) || !strlen($contents)){
-                $errors[$error_name] = $name.'を入力してください';
+                $errors[$error_name] = '<div class="alert alert-danger" role="alert">'.$name.'を入力してください'.'</div>';
             }else if (strlen($contents) > $count) {
-                $errors[$error_name] = $name.'は'.$count.'文字以内で入力してください';
+                $errors[$error_name] = '<div class="alert alert-warning" role="alert">'.$name.'は'.$count.'文字以内で入力してください'.'</div>';
             }else {
                 $rec = $contents;
                 return $rec;
@@ -55,7 +55,7 @@
         // }
 
         //SaveDB
-        var_dump($errors);
+        // var_dump($errors);
         if(count($errors) === 0){
             $sql = 'INSERT INTO `message_table` (`name`,`message`,`created_at`) VALUES (
                 \''.mysql_real_escape_string($name).'\',
@@ -73,16 +73,16 @@
 
 //////////////////////AfterPOSTSend-End//////////////////////
     //MessageOut-Start
-    $select = 'SELECT * FROM `message_table` ORDER BY id';
+    $select = 'SELECT * FROM `message_table` ORDER BY id DESC';
     $result = mysql_query($select,$link);
 
     if($result !== false && mysql_num_rows($result)){ //0件だった場合にrowsが必要
         $list = array();
         while ($post = mysql_fetch_assoc($result)) {
-            $list[] = '<li>'
-            .htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8').'：'
-            .htmlspecialchars($post['message'],ENT_QUOTES,'UTF-8').'：'
-            .htmlspecialchars($post['created_at'],ENT_QUOTES,'UTF-8').'</li>';
+            $list[] = '<tr>'
+            .'<td>'.htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8').'</td>'
+            .'<td>'.htmlspecialchars($post['message'],ENT_QUOTES,'UTF-8').'</td>'
+            .'<td>'.htmlspecialchars($post['created_at'],ENT_QUOTES,'UTF-8').'</td></tr>';
         }
     }
 //////////////////////AfterPOSTSend-End//////////////////////
@@ -93,62 +93,77 @@
 //////////////////////sqlClose-END//////////////////////
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML4.01 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>ひとこと掲示板</title>
-<style type="text/css">
-<!--
-.sub {
-    font-size:10px;
-    color: #aaa;
-}
--->
-</style>
-<script type="text/javascript"><!--
-function CountDownLength( idn, str, mnum ) {
-   document.getElementById(idn).innerHTML = "あと" + (mnum - str.length) + "文字";
-}
-// --></script>
-</head>
-<body>
-<h1>ひとこと掲示板</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ひとこと掲示板</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+    <!--
+    .sub {
+        font-size:10px;
+        color: #aaa;
+    }
+    -->
+    </style>
+    <script type="text/javascript"><!--
+    function CountDownLength( idn, str, mnum ) {
+       document.getElementById(idn).innerHTML = "あと" + (mnum - str.length) + "文字";
+    }
+    // --></script>
+  </head>
+  <body class="container">
+    <h1 style="margin-bottom:20px;">ひとこと掲示板</h1>
 
 <?php
 //////////////////////ErrorOutput-START//////////////////////
 // var_dump($errors);
 if(count($errors) !== 0){
-    echo '<ul>';
     foreach($errors as $error){
-        echo '<li><font color="red">';
-        echo htmlspecialchars($error,ENT_QUOTES,'UTF-8');
-        echo '</font></li>';
+        echo $error;
     }
-    echo '</ul>';
+}else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    echo '<div class="alert alert-success" role="alert">入力されました！</div>';
 }
 //////////////////////ErrorOutput-END//////////////////////
 ?>
 
-<form method="POST" action="index.php">
-    名前：<span id="cdlength1" class="sub">あと20文字</span><br/><input type="text" name="name" onkeyup="CountDownLength('cdlength1' , value , 20);"><br/>
-    <br/>
-    ひとこと：<span id="cdlength2" class="sub">あと200文字</span><br/><textarea name="message" style="width:300px; height:100px;" maxlength="200" onkeyup="CountDownLength('cdlength2' , value , 200);"></textarea><br/>
-    <br/>
-    <input type="submit" value="送信">
+<form method="POST" action="index.php" class="col-md-6">
+    <span id="cdlength1" class="sub">あと20文字</span><br/><input class="form-control" type="text" name="name" placeholder="Enter Name" onkeyup="CountDownLength('cdlength1' , value , 20);"><br/>
+    <span id="cdlength2" class="sub">あと200文字</span><br/><textarea class="form-control" name="message" style="width:300px; height:100px;" placeholder="Enter massage" onkeyup="CountDownLength('cdlength2' , value , 200);"></textarea><br/>
+    <input type="submit" class="btn btn-primary" value="送信">
 </form>
 
+<div class="col-md-6">
 <?php
 //////////////////////MessageBoard-Start//////////////////////
     if($list !== null){
-        echo '<ul>';
+        echo '<table class="table table-striped"><thead><tr><th>Name</th><th>Message</th><th>Time</th></tr></thead>';
         foreach ($list as $value) {
              echo $value;
         }
-        echo '</ul>';
+        echo '</table>';
     }
 //////////////////////MessageBoard-END//////////////////////
 ?>
+</div>
 
-</body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
